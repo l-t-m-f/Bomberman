a8745feff0098b0645fa6674e3dad1f78ba3897b
Em <ltmf1990@protonmail.com>
2025-02-10 16:48:42 -0500

    update to scripts and dependencies

--- begin.bat
+++ begin.bat
@@ -3,10 +3,46 @@ rem This script is to load a default set of submodules. It performs a full clean
 rem This allows us to increment variables during loop
 setlocal enabledelayedexpansion
 
+set PLUTO=https://LetTheMiceFree@dev.azure.com/LetTheMiceFree/pluto/_git/pluto
+rem Check if the repository directory already exists
+    if exist deps\pluto (
+        rem Directory exists, clear it
+        echo Removing existing directory: deps/pluto
+        rmdir /s /q deps\pluto || (
+            echo Failed to remove existing directory: deps/pluto
+            exit /b 1
+        )
+    )
+
+rem Clone Pluto
+    echo Cloning Pluto framework
+    git clone !PLUTO! deps\pluto || (
+        echo Failed to clone repository: deps/pluto
+        exit /b 1
+	)
+		
+set GAME_MODULES=https://LetTheMiceFree@dev.azure.com/LetTheMiceFree/game_modules/_git/game_modules
+rem Check if the repository directory already exists
+    if exist deps\game_modules (
+        rem Directory exists, clear it
+        echo Removing existing directory: deps/game_modules
+        rmdir /s /q deps\game_modules || (
+            echo Failed to remove existing directory: deps/game_modules
+            exit /b 1
+        )
+    )
+	
+rem Clone Game modules
+    echo Cloning additional game modules
+    git clone !GAME_MODULES! deps\game_modules || (
+        echo Failed to clone repository: deps/game_modules
+        exit /b 1
+	)
+
 rem Define the list of repository URLs
 set REPOS=l-t-m-f/SDL-ltmf libsdl-org/SDL_image libsdl-org/SDL_mixer libsdl-org/SDL_net libsdl-org/SDL_ttf dsprenkels/randombytes liteserver/binn Auburn/FastNoiseLite P-p-H-d/mlib SanderMertens/flecs
 
-rem Define the list of submodule names (this list must be in the same order as the URL list)
+rem Define the list of repository names (this list must be in the same order as the URL list)
 set DEPS_LIST=SDL SDL_image SDL_mixer SDL_net SDL_ttf randombytes binn fnl mlib flecs
 
 rem Convert the REPOS list into an array
@@ -23,13 +59,13 @@ for %%B in (%DEPS_LIST%) do (
     set DEPS[!j!]=%%B
 )
 
-rem Loop through each submodule and clear existing directories before adding them again
+rem Loop through each repository and clear existing directories before adding them again
 for /L %%i in (1,1,!i!) do (
-    rem Get the corresponding URL and submodule name
+    rem Get the corresponding URL and repository name
     set URL=https://github.com/!REPO[%%i]!
     set DEPS=!DEPS[%%i]!
 
-    rem Check if the submodule directory already exists
+    rem Check if the repository directory already exists
     if exist deps\!DEPS! (
         rem Directory exists, clear it
         echo Removing existing directory: deps/!DEPS!
@@ -39,12 +75,17 @@ for /L %%i in (1,1,!i!) do (
         )
     )
 
-    rem Clone the submodule
-    echo Cloning submodule: URL=!URL!, Path=deps/!DEPS!
+    rem Clone the repository
+    echo Cloning repository: URL=!URL!, Path=deps/!DEPS!
     git clone !URL! deps\!DEPS! || (
-        echo Failed to clone submodule: deps/!DEPS!
+        echo Failed to clone repository: deps/!DEPS!
         exit /b 1
     )
 )
+
+cd deps
+cd SDL
+git remote set-head origin custom
+echo SDL set to custom branch on l-t-m-f/SDL-ltmf fork
 
 endlocal
--- fetch.bat
+++ fetch.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem Define an array of SDL package names
-set DEPS=SDL SDL_image SDL_mixer SDL_ttf SDL_net binn fnl flecs mlib randombytes
+set DEPS=SDL SDL_image SDL_mixer SDL_ttf SDL_net binn fnl flecs mlib randombytes game_modules Pluto
 
 rem Loop through each package and perform git pull
 for %%P in (%DEPS%) do (
